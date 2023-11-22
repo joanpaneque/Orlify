4782fda01631a27cc220bf5acfe306ed2eed654a
Jhosep Rolando Espinoza Cortez <jespinoza@cendrassos.net>
2023-11-22 15:34:08 +0100

    logout

--- public/index.php
+++ public/index.php
@@ -8,6 +8,9 @@ include "../vendor/autoload.php";
 $container = new \App\Container(__DIR__ . "/../App/config.php");
 $app = new \Emeset\Emeset($container);
 
+
+$app->get("/logout", "\App\Controllers\Logout:logout");
+
 $app->get(Router::DEFAULT_ROUTE, function ($request, $response) {
     $response->setBody("Hola!");
     return $response;
