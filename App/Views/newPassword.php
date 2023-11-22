d9984260f6898bc1042beaf66dde8ec547be6d5d
joan <jpaneque@cendrassos.net>
2023-11-22 22:12:40 +0100

    Design of newPassword page

--- App/Views/newPassword.php
+++ App/Views/newPassword.php
@@ -3,27 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="/main.css">
     <title>Nova contrasenya</title>
 </head>
-<body class="flex items-center justify-center h-screen">
-    <div class="text-center mx-4">
-        <h1 class="text-red-500 text-4xl font-bold mb-4">
-            <?php if ($error) { ?>
-                Error canviant la contrasenya
-            <?php } else { ?>
-                Contrasenya canviada
+<body>
+    <div class="h-screen flex items-center justify-center p-6">
+        <div class="grid gap-6 text-center max-w-lg">
+            <h1 class="title">
+                <?php if ($error) { ?>
+                    Error al recuperar
+                <?php } else { ?>
+                    Contrasenya canviada
+                <?php } ?>
+            </h1>
+            <div class="titleBottomBar"></div>
+            <p class="text-gray-700 grayMessage"><?= $message ?></p>
+            <?php if (!$error) { ?>
+                <p class="text-gray-700">Correu: <?= $email ?></p>
+                <p class="text-gray-700">Contrasenya: <?= $password ?></p> 
             <?php } ?>
-        </h1>
-        <div class="bg-gray-800 h-2 w-16 mx-auto mb-4"></div>
-        <p class="text-gray-700 mb-8"><?= $message ?></p>
-        <?php if (!$error) { ?>
-            <p class="text-gray-700 mb-8">Correu: <?= $email ?></p>
-            <p class="text-gray-700 mb-8">Contrasenya: <?= $password ?></p>
-        <?php } ?>
-        <form action="/login" method="GET">
-            <button type="submit" class="bg-red-500 text-white px-6 py-2 rounded">Iniciar sessió</button>
-        </form>
+            <a href="/login" class="roundedRedButton">Iniciar sessió</a>
+        </div>
     </div>
 </body>
 </html>
