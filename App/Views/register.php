730c6dc3f7fff3610b0103cf5c8be3dc753f8785
Jhosep Rolando Espinoza Cortez <jespinoza@cendrassos.net>
2023-11-27 16:27:35 +0100

    feature-register-0.2

--- App/Views/register.php
+++ App/Views/register.php
@@ -6,22 +6,20 @@
     <link rel="stylesheet" href="/main.css">
     <script src="/js/bundle.js"></script>
     <meta allocation="inputBlock">
-    <meta allocation="hola">
+   
 
     <title>Document</title>
 </head>
 
-
-
 <body class="bg-gray-100">
   <div class=" flex flex-row items-center justify-center text-center relative">
-  <p class="absolute top-0 left-0 font-bold">Orlify</p>
+  <a href="/login"><p class="absolute top-0 left-0 font-bold">Orlify</p></a>
     <div class="grid gap-6 text-center flex-1">
       <h1 class="titlered">
         Registrar-se
       </h1>
       <div class="titleBottomBar"></div> 
-      <form action="/recover/sendMail" method="POST" class="mt-6 flex-grow">
+      <form action="/recover/sendMail" method="POST" class="mt-6 flex-grow hhh">
         <div class="inputBlock">
           <label for="nom">Nom</label>
           <input type="text" name="nom" class="inputField" required>
@@ -42,9 +40,9 @@
           <label for="password">Confirmar Contrasenya</label>
           <input type="password" name="password" class="inputField" required>
         </div>
-        <a href="/recover"><button type="submit" class="roundedRedButton">Registrar-se</button></a>
+        <a href="/login"><button type="submit" class="roundedRedButton">Registrar-se</button></a>
       </form>
-      <p class="font-bold">Ja tens compte? <a href="/recover" class="text-red-600">Inicia Sessió</a></p>
+      <p class="font-bold">Ja tens compte? <a href="/login" class="text-red-600">Inicia Sessió</a></p>
     </div>
                 <div class="hidden lg:flex min-h-screen w-1/2 flex flex-col bg-red-500 items-center justify-center text-center">
                 <h1 class="title2">
@@ -54,7 +52,7 @@
                 <p class="text-white mt-10">Si ja ets un usuari de Orlify</p>
                 <p class="text-white ">inicia sessió des de la pàgina</p>
                 <p class="text-white ">d’inici de sessió</p>
-            <a href="/recover"><button type="submit" class="roundedWhiteButton">Iniciar Sessió</button></a>
+            <a href="/login"><button type="submit" class="roundedWhiteButton">Iniciar Sessió</button></a>
                 </div>
   </div>
 </body>
--- App/js/index.js
+++ App/js/index.js
@@ -1,8 +1,7 @@
 import Emesis from "./Emesis/Emesis.js";
 // Allocators
 import inputBlock from "./Allocators/inputBlock.js";
-
 // "reciever": allocator
 const emesis = new Emesis({
-    "inputBlock": inputBlock
+    "inputBlock": inputBlock,   
 });
