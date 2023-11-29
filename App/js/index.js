7d7b0931848d8c3df03afd1306aa7c18a26e0aa6
joan <jpaneque@cendrassos.net>
2023-11-29 16:53:51 +0100

    Admin page

--- App/js/index.js
+++ App/js/index.js
@@ -1,11 +1,6 @@
 import Emesis from "./Emesis/Emesis.js";
 // Allocators
 import inputBlock from "./Allocators/inputBlock.js";
-<<<<<<< HEAD
-// "reciever": allocator
-const emesis = new Emesis({
-    "inputBlock": inputBlock,   
-=======
 import inputSelect from "./Allocators/inputSelect.js";
 import admin from "./Allocators/admin.js";
 
@@ -14,5 +9,4 @@ const emesis = new Emesis({
     "inputBlock": inputBlock,
     "inputSelect": inputSelect,
     "admin": admin
->>>>>>> feature-admin-index2
 });
