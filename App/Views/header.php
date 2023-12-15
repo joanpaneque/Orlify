7d09dcab14ab055c9df46c83b1900586933e0bcc
Jhosep Rolando Espinoza Cortez <jespinoza@cendrassos.net>
2023-12-15 19:35:08 +0100

    view-header

--- App/Views/header.php
+++ App/Views/header.php
@@ -1,33 +1,131 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta allocation="groups">
-    <title>Orlify</title>
-    <script src="/js/bundle.js"></script>
-    <link rel="stylesheet" href="/main.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta allocation="groups">
+        <title>Orlify</title>
+        <script src="/js/bundle.js"></script>
+        <link rel="stylesheet" href="/main.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<body class="bg-gray-100">
+    <body class="bg-gray-100 flex flex-col min-h-screen">
 
-    <div class="bg-red-500 p-4 flex items-center">
-        <a class="text-white mb-2 mr-4" href="/login">
-            <p>Orlify</p>
-        </a>
-        <a class="text-white ml-auto mb-2" href="/login">
-            <p>Tencar Sesión</p>
-        </a>
-    </div>
-    <h1 class="mt-8 text-center text-4xl md:text-6xl font-extrabold text-red-500">
-        ¡Administra les teves clases!
-    </h1>
+        <div class="bg-red-500 p-4 flex items-center">
+            <a class="text-white mb-2 mr-4" href="/login">
+                <p>Orlify</p>
+            </a>
+            <a class="text-white ml-auto mb-2" href="/login">
+                <p>Iniciar Sesión</p>
+            </a>
+        </div>
+        <div>
+            <h1 class="text-dark-500 mt-3 mb-5 text-center text-4xl md:text-6xl font-extrabold ">
+                Benvinguts a l'institut Cendrassos!
+            </h1>
+        </div>
+
+        <div>
 
 
 
-   
-</body>
+            <!-- Slider -->
+            <div id="default-carousel" class="relative w-full h-full" data-carousel="slide">
+                <!-- Carousel wrapper -->
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    <!-- Item 1 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="/icons/cen1.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    </div>
+                    <!-- Item 2 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="/icons/cen2.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    </div>
+                    <!-- Item 3 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="/icons/cen3.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    </div>
+                    <!-- Item 4 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="/icons/cen4.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    </div>
+                    <!-- Item 5 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="/icons/cen5.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    </div>
+                </div>
+                <!-- Slider indicators -->
+                <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+                </div>
+                <!-- Slider controls -->
+                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
+            <div class="text-center">
+                <h1 class="mt-3 mb-5 text-center text-4xl md:text-6xl font-extrabold text-dark-500">
+                    Orles disponibles per els grups de l'institut
+                </h1>
+                <a href="/login">
+                    <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
+                        Visualitzar Orles
+                    </button>
+                </a>
+
+            </div>
+        </div>
+
+        <!-- Div con fondo negro y texto blanco -->
 
 
-</html>
+
+
+
+        <!-- fotter -->
+        <div class="bottom-0 left-0 w-full bg-red-500 p-4 flex items-center fixed">
+    <a class="text-white mb-2 mr-4 flex items-center" href="/login">
+        <p>Contacte: Orlify.es</p>
+        
+    </a>
+    <a class="text-white ml-auto mb-2 flex items-center" href="/login">
+        <p>Redes</p>
+        <i class="fab fa-instagram ml-2 mr-1"></i>
+        <i class="fa-brands fa-twitter mr-1"></i>
+        <i class="fa-brands fa-telegram mr-1"></i>
+    </a>
+</div>
+
+
+
+
+
+
+        </div>
+
+
+    </body>
+
+
+    </html>
